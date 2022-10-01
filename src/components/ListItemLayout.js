4fb602a7018a386e73273dfb66bde35a08678cb6
wooinwoo <wooin1001@gmail.com>
2022-10-02 00:50:45 +0900

    key 값 추가

--- src/components/ListItemLayout.js
+++ src/components/ListItemLayout.js
@@ -2,7 +2,7 @@ import styles from "../components/componentStyles/ListItemLayout.module.css";
 
 export default function ListItemLayout({ children, imgSrc, title }) {
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.wrapper} key={imgSrc + title + new Date()}>
       <img src={imgSrc} alt="" className={styles.img} />
       <div className={styles.info}>
         <p className={styles.title}>{title}</p>
